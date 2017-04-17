5978994f9677526b287845920b7d684f9ce046e0
Paul Gross <pgross@maryville.edu>
2017-04-17 15:14:37 -0500

    Initial commit
    Blank application

---
+++ Social Inclusion App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Inclusion App
+//
+//  Created by Gross, Paul on 4/17/17.
+//  Copyright © 2017 Maryville Information Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
