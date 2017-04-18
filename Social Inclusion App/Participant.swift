d0045b2050005e63fc520a16bb1e0c41721c46f5
Unknown <jzaiss1@live.maryville.edu>
2017-04-17 20:48:11 -0500

    Added Participant struct
    Added a struct to manage participant preferences

---
+++ Social Inclusion App/Participant.swift
@@ -0,0 +1,17 @@
+//
+//  Participant.swift
+//  TeamProjectDraft
+//
+//  Created by John Zaiss on 4/11/17.
+//  Copyright © 2017 John Zaiss. All rights reserved.
+//
+
+import UIKit
+
+// Still a lot of unknown about the needs of the participant entiry and what methods are required
+
+struct Participant {
+    var name: String
+    var email: String
+    var code: String
+}
