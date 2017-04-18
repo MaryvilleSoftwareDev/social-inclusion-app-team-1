f6a87259f33773148f877955a06b0c5022719964
Unknown <jzaiss1@live.maryville.edu>
2017-04-17 23:32:13 -0500

    Adding the ActivityLog class
    This is the updated version we worked on in class

---
+++ Social Inclusion App/ActivityLog.swift
@@ -0,0 +1,56 @@
+//
+//  ActivityLog.swift
+//  TeamProjectDraft
+//
+//  Created by John Zaiss on 4/11/17.
+//  Copyright © 2017 John Zaiss. All rights reserved.
+//
+
+import UIKit
+
+enum EmotionEmoji {
+    
+    case positive
+    case neutral
+    case negative
+    
+    var display: String {
+        
+        switch self {
+        case .positive: return "😀"
+        case .neutral: return "😐"
+        case .negative: return "☹️"
+        }
+        
+    }
+}
+
+class ActivityLogItem: Activity {
+    
+    var dateCompleted: Date
+    var reaction: EmotionEmoji
+    var recording: String? //audio file
+    
+    init(activity: Activity, totalTime: Int, reaction: EmotionEmoji, recording: String?) {
+        
+        self.dateCompleted = Date()
+        self.reaction = reaction
+        self.recording = recording // probably better to have function in this class to start the recorder
+        super.init()
+        
+        /*
+         Additiona functions needed are
+         createAudioRecording
+         */
+        
+    }
+    
+    // placeholder function
+    func setEmotionEmoji() -> EmotionEmoji {
+        
+        return .neutral
+        
+    }
+    
+    
+}
