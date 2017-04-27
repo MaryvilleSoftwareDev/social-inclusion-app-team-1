5568e1fbb8f7af3ab12855c9944493de748d2092
Unknown <jzaiss1@live.maryville.edu>
2017-04-26 19:53:27 -0500

    Added aCoder and aDecoder to ActivityLogItem class

--- Social Inclusion App/ActivityLog.swift
+++ Social Inclusion App/ActivityLog.swift
@@ -53,7 +53,8 @@ struct InstructionTimer {
     }
 }
 
-class ActivityLogItem {
+class ActivityLogItem: NSObject, NSCoding {
+    
     
     var dateCompleted: Date?
     var reaction: EmotionEmoji
@@ -63,7 +64,7 @@ class ActivityLogItem {
     var instructionCode: String
     var instructionTimer = [InstructionTimer]()
     
-    init() {
+    override init() {
         
         self.dateCompleted = nil
         self.reaction = .none
@@ -75,11 +76,44 @@ class ActivityLogItem {
     }
     
     
-    
     // placeholder function
+    required init(coder aDecoder: NSCoder) {
+        dateCompleted = aDecoder.decodeObject(forKey: "dateCompleted") as? Date
+        reaction = (aDecoder.decodeObject(forKey: "reaction") as? EmotionEmoji)!
+        recording = aDecoder.decodeObject(forKey: "recording") as? String
+        participantCode = aDecoder.decodeObject(forKey: "participantCode") as! String
+        activityCode = aDecoder.decodeObject(forKey: "activityCode") as! String
+        instructionCode = aDecoder.decodeObject(forKey: "instructionCode") as! String
+        instructionTimer = [aDecoder.decodeObject(forKey: "instructionTimer") as! InstructionTimer]
+        
+        super.init()
+    }
+
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(dateCompleted, forKey: "dateCompleted")
+        aCoder.encode(reaction, forKey: "reaction")
+        aCoder.encode(recording, forKey: "recording")
+        aCoder.encode(participantCode, forKey: "participantCode")
+        aCoder.encode(activityCode, forKey: "activityCode")
+        aCoder.encode(instructionCode, forKey: "instructionCode")
+        aCoder.encode(instructionTimer, forKey: "instructionTimer")
+        
+        
+    }
     func setEmotionEmoji() -> EmotionEmoji {
         
         return .neutral
+        
+    }
+    
+    func readActivityLog() {
+        
+    }
+    
+    func saveActivityLog() {
+        
+        
         
     }
     
