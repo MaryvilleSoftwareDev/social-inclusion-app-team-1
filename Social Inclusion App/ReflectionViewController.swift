2497ce46924c91ac58d585b975047b24818a3308
Unknown <auer.ryan@yahoo.com>
2017-04-30 21:12:33 -0500

    VIew controller Responsive

--- Social Inclusion App/ReflectionViewController.swift
+++ Social Inclusion App/ReflectionViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class ReflectionViewController: UIViewController, UITextViewDelegate {
+import AVFoundation
+
+
+
+
+class ReflectionViewController: UIViewController, UITextViewDelegate, AVAudioRecorderDelegate  {
     @IBOutlet weak var summaryTextView: UITextView!
     @IBOutlet weak var submitReflectionButton: UIButton!
     @IBOutlet weak var negativeEmotion: UIButton!
     @IBOutlet weak var neutralEmotion: UIButton!
     @IBOutlet weak var positiveEmotion: UIButton!
+    
+    //sound variables//
+    
+//    var recordButton: UIButton!
+//    var recordingSession: AVAudioSession!
+//    var audioRecorder: AVAudioRecorder!
     
     var reflectionActivity: ActivityLogItem!
     
@@ -57,7 +68,28 @@ class ReflectionViewController: UIViewController, UITextViewDelegate {
         summaryTextView.layer.borderColor = UIColor.black.cgColor
         
         summaryTextView.delegate = self
-    }
+        
+        
+        //sound addition here//
+        
+//        recordingSession = AVAudioSession.sharedInstance()
+//        
+//        do {
+//            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+//            try recordingSession.setActive(true)
+//            recordingSession.requestRecordPermission() { [unowned self] allowed in
+//                DispatchQueue.main.async {
+//                    if allowed {
+//                        self.loadRecordingUI()
+//                    } else {
+//                        // failed to record!
+//                    }
+//                }
+//            }
+//        } catch {
+//            // failed to record!
+//        }
+//    }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
         if summaryTextView.textColor == UIColor.lightGray {
@@ -72,9 +104,74 @@ class ReflectionViewController: UIViewController, UITextViewDelegate {
         }
     }
     
-    @IBAction func submiteReflectionButtonPressed(_ sender: Any) {
+    func submiteReflectionButtonPressed(_ sender: Any) {
         reflectionActivity.recording = summaryTextView.text
         reflectionActivity.dateCompleted = Date()
         //still have to make it so that this relfectionActivity is logged onto the server
     }
+    
+//    //Audio Recodring//
+//    
+//    func loadRecordingUI() {
+//        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+//        recordButton.setTitle("Tap to Record", for: .normal)
+//        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.title1)
+//        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+//        view.addSubview(recordButton)
+//    }
+//    
+//    func startRecording() {
+//        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+//        
+//        let settings = [
+//            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+//            AVSampleRateKey: 12000,
+//            AVNumberOfChannelsKey: 1,
+//            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+//        ]
+//        
+//        do {
+//            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+//            audioRecorder.delegate = self
+//            audioRecorder.record()
+//            
+//            recordButton.setTitle("Tap to Stop", for: .normal)
+//        } catch {
+//            finishRecording(success: false)
+//        }
+//    }
+//    
+//    func getDocumentsDirectory() -> URL {
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        let documentsDirectory = paths[0]
+//        return documentsDirectory
+//    }
+    //stop recording button//
+    
+//    func finishRecording(success: Bool) {
+//        audioRecorder.stop()
+//        audioRecorder = nil
+//        
+//        if success {
+//            recordButton.setTitle("Tap to Re-record", for: .normal)
+//        } else {
+//            recordButton.setTitle("Tap to Record", for: .normal)
+//            // recording failed :(
+//        }
+//    }
+//    
+//    func recordTapped() {
+//        if audioRecorder == nil {
+//            startRecording()
+//        } else {
+//            finishRecording(success: true)
+//        }
+//    }
+//    
+//    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+//        if !flag {
+//            finishRecording(success: false)
+//        }
+//    }
+}
 }
